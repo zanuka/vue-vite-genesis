cdf72eba819011c8baf53310825b9e2dae8c3cdd
zanuka <git@zanuka.com>
2021-07-24 02:38:18 +1200

    cleanup :: fixup tests

--- src/components/HelloWorld.spec.ts
+++ src/components/HelloWorld.spec.ts
@@ -4,7 +4,7 @@ import HelloWorld from './HelloWorld.vue'
 describe('HelloWorld', () => {
   it('should display header text', () => {
     const msg = 'Hello Vue 3'
-    const wrapper = mount(HelloWorld, { props: { msg } })
+    const wrapper = mount(HelloWorld as any, { props: { msg } })
 
     expect(wrapper.find('h1').text()).toEqual(msg)
   })
