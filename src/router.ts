15e10b88d3592dc7427b193c765780c9d44fd126
zanuka <git@zanuka.com>
2024-10-10 01:32:12 -0700

    brings repo up to speed : mods and tweaks

--- src/router.ts
+++ src/router.ts
@@ -1,26 +1,29 @@
-import { createRouter, createWebHashHistory, RouteParams } from 'vue-router'
-import Home from './pages/Home.vue'
+import { createRouter, createWebHashHistory, RouteParams } from 'vue-router';
+import Home from './pages/Home.vue';
 
-export type AppRouteNames = 'home'
+export type AppRouteNames = 'home';
 
 export const router = createRouter({
-  history: createWebHashHistory(),
-  routes: [
-    {
-      name: 'home',
-      path: '/',
-      component: Home,
-    },
-  ],
-})
+	history: createWebHashHistory(),
+	routes: [
+		{
+			name: 'home',
+			path: '/',
+			component: Home
+		}
+	]
+});
 
-export function routerPush (name: AppRouteNames, params?: RouteParams): ReturnType<typeof router.push> {
-  if (params !== undefined) {
-    return router.push({
-      name,
-      params,
-    })
-  } else {
-    return router.push({ name })
-  }
+export function routerPush(
+	name: AppRouteNames,
+	params?: RouteParams
+): ReturnType<typeof router.push> {
+	if (params !== undefined) {
+		return router.push({
+			name,
+			params
+		});
+	} else {
+		return router.push({ name });
+	}
 }
